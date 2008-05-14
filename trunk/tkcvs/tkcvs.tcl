24d3a50bcbafa7938ab9f6a90f12cc6fa0b85974
dorothyr <dorothyr@919ae00d-1f00-0410-8caf-94996817b07d>
2008-05-14 17:57:52 +0000

    Add diagram search to branch browser. Roll version to 8.1.1.

--- trunk/tkcvs/tkcvs.tcl
+++ trunk/tkcvs/tkcvs.tcl
@@ -64,7 +64,7 @@ set cvscfg(bitmapdir) [file join $TclRoot tkcvs bitmaps]
 #puts "TCDIR $TCDIR"
 #puts "BITMAPDIR $cvscfg(bitmapdir)"
 
-set cvscfg(version) "8.1"
+set cvscfg(version) "8.1.1"
 
 if {! [info exists cvscfg(editorargs)]} {
   set cvscfg(editorargs) {}
