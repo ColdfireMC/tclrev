a040a7a837e91c16554aa2551db368f13983c169
dorothyr <dorothyr@919ae00d-1f00-0410-8caf-94996817b07d>
2019-07-26 18:39:10 +0000

    Change behavior of cvscfg(toomany_tags) for Subversion. Instead of all-or-nothing, it now processes the most recent up to the limit

--- trunk/tkcvs/tkcvs.tcl
+++ trunk/tkcvs/tkcvs.tcl
@@ -73,7 +73,7 @@ set cvscfg(bitmapdir) [file join $TclRoot tkcvs bitmaps]
 #puts "TCDIR $TCDIR"
 #puts "BITMAPDIR $cvscfg(bitmapdir)"
 
-set cvscfg(version) "9.1.5"
+set cvscfg(version) "9.1.6"
 
 if {! [info exists cvscfg(editorargs)]} {
   set cvscfg(editorargs) {}
