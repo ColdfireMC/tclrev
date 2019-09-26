93c5d02047c3fc14ff59493a7a942216113482e3
dorothyr <dorothyr@919ae00d-1f00-0410-8caf-94996817b07d>
2019-09-26 23:51:01 +0000

    put the Git Tools menu back where it was

--- tkcvs/workdir.tcl
+++ tkcvs/workdir.tcl
@@ -853,7 +853,7 @@ proc setup_dir { } {
     gen_log:log D "CONFIGURE GIT MENUS"
     .workdir.menubar insert [expr {$filemenu_idx + 1}] cascade -label "GIT" \
       -menu .workdir.menubar.git
-    .workdir.menubar insert [expr {$filemenu_idx + 2}] cascade -label "Git Tools" \
+    .workdir.menubar insert [expr {$filemenu_idx + 4}] cascade -label "Git Tools" \
       -menu .workdir.menubar.gitopts
     .workdir.top.bmodbrowse configure -image Modules_git -command modbrowse_run
     .workdir.top.lmodule configure -text "path"
