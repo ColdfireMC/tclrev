3c74177d03deda737bfdf71599a219811003fc54
Alejandro Estay <alejandro.estay@gmail.com>
2022-05-21 19:31:42 -0400

    clean main, fix entry

--- tclrev/tkrev.tcl
+++ tclrev/tkrev.tcl
@@ -1,144 +1,4 @@
-
-proc make_src_ctrled { } {
-	global repo
-	global cvsglb ""
-	global cvscfg ""
-	switch repotype {
-        svn {
-            ACTION
-        }
-        git {
-            ACTION2
-        }
-		vcs {
-            ACTION2
-        }
-		rcs {
-			
-		}
-		default {
-			puts "error "
-		}	
-    }
-}
-proc checkout_proj { proj_url } {
-	global repo
-	global cvsglb ""
-	global cvscfg ""
-	switch repotype {
-        svn {
-            svn_check
-        }
-        git {
-            ACTION2
-        }
-		vcs {
-            ACTION2
-        }
-		rcs {
-			
-		}
-		
-		default {
-			puts "error "
-		}	
-    }
-}
-proc checkout_files { repo_url repotype file_list} {
-	global repo
-	global cvsglb ""
-	global cvscfg ""
-	switch repotype {
-        svn {
-            svn_check
-        }
-        git {
-            ACTION2
-        }
-		vcs {
-            ACTION2
-        }
-		rcs {
-			
-		}
-		default {
-		puts "error "
-		}	
-    }
-}
-proc repo_file_delete { files args } {
-    global repo
-	global cvsglb ""
-	global cvscfg ""
-	switch repotype {
-        svn {
-            svn_check
-        }
-        git {
-            ACTION2
-        }
-		vcs {
-            ACTION2
-        }
-		rcs {
-			
-		}	
-		default {
-			puts "error "
-		}
-    }
-}
-proc commit_project { repotype proj_struct msg} {
-	global repo
-	global cvsglb ""
-	global cvscfg ""
-	switch repotype {
-        svn {
-            svn_check
-        }
-        git {
-            ACTION2
-        }
-		vcs {
-            ACTION2
-        }
-		rcs {
-			
-		}
-		default {
-			puts "error "
-		}	
-    }
-}
-proc commit_files { repotype file_list msg args } {
-	global repo
-	global cvsglb ""
-	global cvscfg ""
-	switch repotype {
-        svn {
-            svn_commit msg args
-        }
-        git {
-            ACTION2
-        }
-		vcs {
-            ACTION2
-        }
-		rcs {
-			
-		}
-		default {
-			puts "error "
-		}	
-    }
-}
-proc redef_project_commands { } {
-	
-}
-#proc autobuild_project_from_repo { } {
-#	
-# }
-proc srcctrlchk {} {
+proc srcctrlchk {} { 
 	global env
 	global cvscfg
 	global cvsglb
@@ -199,16 +59,17 @@ proc srcctrlchk {} {
 		set cvsglb(vcs) cvs
     }
 }
-proc usgprint {} {
+proc usgprint {} { 
 	# Command line options	
 	set usage "Usage:"
 	global argv
 	append usage "\n tkrev \[-root <cvsroot>\] \[-win workdir|module|merge\]"
 	append usage "\n tkrev \[-log|blame <file>\]"
 	append usage "\n tkrev <file> - same as tkrev -log <file>"
-}	
+}	 
 	
-proc arg_parse {argv} {
+proc arg_parse { argv } {
+	
  if {[info exists argv]}	{
 		for {set i 0} {$i < [llength $argv]} {incr i} {
 			set arg [lindex $argv $i]
@@ -241,7 +102,7 @@ proc arg_parse {argv} {
 					incr i
 				}
 				{^--*h.*} {
-					puts $usage
+					usgprint
 					exit 0
 				}
 				{^\.*\w*} {
@@ -258,17 +119,11 @@ proc arg_parse {argv} {
 						} else {
 							set cvscfg(startwindow) log
 							set lcfile $arg; incr i
-						}
-					}
-					
-				} default {
-					puts $usage
-					}
-			}
-		}
-	}
-	else {
-		puts $usage
+					} 
+ 			} 
+				}
+			} 
+	 }
 	}
 }
 proc tkrevinit { } {
@@ -281,20 +136,17 @@ proc tkrevinit { } {
 	global cvsglb
 	global Script
 	set TclExe [info nameofexecutable]
-	    
+	set auto_path [lappend auto_path [file dirname $TclExe]]    
 	if {[info exists TclRoot]} {
 		    return 1
-		}    
-	set Script [info script]
-	set ScriptTail [file tail $Script]
-	puts "Tail $ScriptTail"
-	if {[file type $Script] == "link"} {
+		}
+		if {[file type $Script] == "link"} {
 			#puts "$Script is a link"
 			set ScriptBin [file join [file dirname $Script] [file readlink $Script]]
 	   	} else {
 			set ScriptBin $Script
 		}
-	puts "ScriptBin $ScriptBin"
+
 	set TclRoot [file join [file dirname $ScriptBin]]
 	puts "TclRoot $TclRoot"
 	if {$TclRoot == "."} {
@@ -303,42 +155,6 @@ proc tkrevinit { } {
 	puts "TclRoot $TclRoot"
 	set TclRoot [file join [file dirname $TclRoot] "lib"]
 	puts "TclRoot $TclRoot"
-	# allow runtime replacement
-	if {[info exists env(TCLROOT)]}	{
-	  set TclRoot $env(TCLROOT)
-	}
-	puts "TclRoot $TclRoot"
-	set cvscfg(version) "9.4.2"
-	set TCDIR [file join $TclRoot tkrev]      
-	if {! [info exists cvscfg(editorargs)]}	{
-		set cvscfg(editorargs) {}
-	}
-	set auto_path [linsert $auto_path 0 $TCDIR]
-	set cvscfg(allfiles) false
-	if {! [info exists cvscfg(startwindow)]} {
-		set cvscfg(startwindow) "workdir"
-	}
-	set cvscfg(auto_tag) false
-	set cvscfg(econtrol) false
-	set cvscfg(use_cvseditor) false
-    set maxdirs 15
-    set dirlist {}
-    set totaldirs 0
-    puts "TCDIR $TCDIR"      
-    # Orient ourselves
-    if { [info exists env(HOME)] } {
-		set cvscfg(home) $env(HOME)
-    } else {
-		set cvscfg(home) "~"
-    }
-    if { [info exists env(USER)] } {
-		set cvscfg(user) $env(USER)
-    } elseif { [info exists env(USERNAME)] } {
-		# Windows
-		set cvscfg(user) $env(USERNAME)
-	} else {
-		set cvscfg(user) ""
-	}
 	 # Read in defaults
 	if { [file exists [file join $TCDIR tkrev_def.tcl]] }	{
 		source [file join $TCDIR tkrev_def.tcl]
@@ -360,10 +176,7 @@ proc tkrevinit { } {
 	# Set some defaults
 	set cvsglb(commit_comment) ""
 	set cvsglb(cvs_version) "" 
-	if {$cvscfg(use_cvseditor) && ![info exists cvscfg(terminal)]} {
-		cvserror "cvscfg(terminal) is required if cvscfg(use_cvseditor) is set \a"
-	}
-	srcctrlchk  
+	srcctrlchk    
 }
 ##
 # tclrev entry
@@ -373,7 +186,7 @@ array set cvscfg ""
 set repotype_default ""
 set repotype ""
 if {[info exists argv] } { 
-	tkrevinit
+		tkrevinit
 } else {
-	usgprint
+		usgprint
 }
